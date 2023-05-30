65a115872f6c5fd122d8c0924ac273fd2815f467
hanz <mshanz@hotmail.com>
2023-05-30 14:02:24 -0700

    add promise

--- FullStackUI/FullStck.UI/src/app/components/employees/employees-list/employees-list.component.ts
+++ FullStackUI/FullStck.UI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -13,6 +13,28 @@ export class EmployeesListComponent implements OnInit {
 
   }
   ngOnInit(): void{
+
+    let p1 = new Promise( (resolve, reject) => {
+      let x = 1+2;
+      if(x == 3){resolve("soap");
+      }else{reject("soap reject");}
+    });
+
+    let p2 = new Promise( (resolve, reject) => {
+      let x = 1+2;
+      if(x == 3){resolve("soap");
+      }else{reject("soap reject");}
+    });
+
+    p1.then((data)=>{console.log("got success message " + data);})
+    .catch((data)=>{console.log("got error message " + data);});
+
+    Promise.all([p1,p2]).then(([data1,data2]) => console.log("got success message " + data1 + " " + data2))
+    .catch(([data1,data2]) => console.log("got failed message " + data1 + " " + data2));
+
+    Promise.any([p1,p2]).then((data)=>{console.log("got success message " + data);})
+    .catch((data)=>{console.log("got error message " + data);});
+
     this.employeesService.getAllEmployees()
     .subscribe({
       next: (employees) => {
