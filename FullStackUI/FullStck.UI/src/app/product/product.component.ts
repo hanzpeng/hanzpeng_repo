07a1a139c0ad8a7f66b3235fcdbb1f27c93700cc
hanz <mshanz@hotmail.com>
2023-05-31 15:42:47 -0700

    retrieve route params

--- FullStackUI/FullStck.UI/src/app/product/product.component.ts
+++ FullStackUI/FullStck.UI/src/app/product/product.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
-
+export class ProductComponent implements OnInit {
+  constructor(private route:ActivatedRoute){}
+  productId$: any; 
+  ngOnInit(): void {
+    this.productId$ = this.route.paramMap
+    .pipe(map(params => params.get('id')));
+  }
 }
