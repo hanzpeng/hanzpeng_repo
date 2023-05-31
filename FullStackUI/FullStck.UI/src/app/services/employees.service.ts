c49c575f7c824fdda57c0a54e3eb8608b3692a2d
hanz <mshanz@hotmail.com>
2023-05-31 13:04:11 -0700

    add http error handling

--- FullStackUI/FullStck.UI/src/app/services/employees.service.ts
+++ FullStackUI/FullStck.UI/src/app/services/employees.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Employee } from '../models/employee.model';
 import { environment } from 'src/environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, catchError, retry, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,17 +12,32 @@ export class EmployeesService {
   constructor(private http: HttpClient) { 
   }
   
-  getGithub() : Observable<any>{
-    const path = 'https://api.github.com/search/repositories?q=hanzpeng';
-    return this.http.get(path);
-  }
-
   getAllEmployees() : Observable<Employee[]>{
-    return this.http.get<Employee[]>(this.baseApiUrl+ 'api/employee');
+    return this.http.get<Employee[]>(this.baseApiUrl+ 'api/employee')
+    .pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
   }
 
   addEmployee(addEmployeeRequest:Employee) : Observable<Employee>{
     addEmployeeRequest.id = '00000000-0000-0000-0000-000000000000';
     return this.http.post<Employee>(this.baseApiUrl+ 'api/employee',addEmployeeRequest);
+  }
+
+  getGithub() : Observable<any>{
+    const path = 'https://api.github.com/search/repositories?q=hanzpeng';
+    return this.http.get(path).pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
+  }
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('A client-side or network error occurred:', error.error);
+    } else {
+      console.error(`Backend returned code ${error.status}, body was: `, error.error);
+    }
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
--- FullStackUI/FullStck.UI/src/app/token.interceptor.ts
+++ FullStackUI/FullStck.UI/src/app/token.interceptor.ts
@@ -9,9 +9,7 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-
   constructor() {}
-
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const API_KEY = "123";
     const newRequest = request.clone({
