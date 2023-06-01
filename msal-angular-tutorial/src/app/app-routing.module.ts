086774e5a6b350aef55e7608727cc2dc741e8b00
hanz <mshanz@hotmail.com>
2023-06-01 13:54:08 -0700

    update AppRoutingModule

--- msal-angular-tutorial/src/app/app-routing.module.ts
+++ msal-angular-tutorial/src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { BrowserUtils } from "@azure/msal-browser";
+import { HomeComponent } from "./home/home.component";
+import { ProfileComponent } from "./profile/profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "profile",
+    component: ProfileComponent,
+  },
+  {
+    path: "",
+    component: HomeComponent,
+  },
+];
+
+const isIframe = window !== window.parent && !window.opener;
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      // Don't perform initial navigation in iframes or popups
+      initialNavigation:
+        !BrowserUtils.isInIframe() && !BrowserUtils.isInPopup()
+          ? "enabledNonBlocking"
+          : "disabled", // Set to enabledBlocking to use Angular Universal
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
