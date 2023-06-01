de05fc9c9bb3dcddbc991dd2cbabffc771ae1896
hanz <mshanz@hotmail.com>
2023-06-01 11:23:20 -0700

    add angular core

--- AngularCore/AngularCore/Program.cs
+++ AngularCore/AngularCore/Program.cs
@@ -8,16 +8,27 @@ namespace AngularCore
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-            app.UseAuthorization();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
 
 
-            app.MapControllers();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller}/{action=Index}/{id?}");
+
+            app.MapFallbackToFile("index.html");
 
             app.Run();
         }
